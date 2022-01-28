2a1166f722bad2502298b3b69a42956e37ae0762
Sebastian <sebastian.yii.xh@gmail.com>
2022-01-28 21:07:51 +0800

    Added gitignore to project

--- node_modules/@govtechsg/dnsprove/dist/ts/common/error.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum DnsproveStatusCode {
-    IDNS_QUERY_ERROR_GENERAL = 0
-}
-export declare class CodedError extends Error {
-    code: number;
-    codeString: string;
-    constructor(message: string, code: DnsproveStatusCode, codeString: string);
-}
